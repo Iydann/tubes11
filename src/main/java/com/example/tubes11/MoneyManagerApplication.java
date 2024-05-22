ed47b516657a10aaed4ae25c3c128b121aebdf4c
ALIEFRACHMATTUL <11231007@student.itk.ac.id>
2024-05-23 01:49:35 +0800

    apdet menu

--- src/main/java/com/example/tubes11/MoneyManagerApplication.java
+++ src/main/java/com/example/tubes11/MoneyManagerApplication.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MoneyManagerApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MoneyManagerApplication.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 620, 390);
         stage.setTitle("");
         stage.setScene(scene);
